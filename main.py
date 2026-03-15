e383be64fadc21d94adf7971a7dab9694785c246
Yaa Asantewaa <antwiyaa17@gmail.com>
2026-03-15 18:51:03 +0000

    Update player characters in main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # This is the main entry point of the project
-from game import play, mrugesh, abbey, quincy, kris, human, random_player
+from game import play, poki, roki, groki, woki, human, random_player
 from rock_pasci import player
 from unittest import main
 
-play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+play(player, groki, 1000)
+play(player, roki, 1000)
+play(player, woki, 1000)
+play(player, poki, 1000)
 
 # Uncomment line below to play interactively against a bot:
-play(human, abbey, 20, verbose=True)
+play(human, roki, 20, verbose=True)
 
 # Uncomment line below to play against a bot that plays randomly:
 # play(human, random_player, 1000)
